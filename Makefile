taudac/id-eeprom

commit e6ccb51d81bf8d02922f70abfe43d13397d02b91

eep: Add commit hash as custom data

Makefile
@@ -27,6 +27,9 @@ endef
 
 all: taudac.eep
 
+taudac-info.txt:
+	git describe --dirty > taudac-info.txt
+
 blank.eep:
 	dd if=/dev/zero ibs=1k count=8 of=blank.eep
 
@@ -35,9 +38,9 @@ taudac.dtbo: taudac-overlay.dts
 	$(CC) -E -P -I . -x assembler-with-cpp taudac-overlay.dts -o taudac-overlay.dts.i
 	$(DTC) -@ -H epapr -I dts -O dtb -o taudac.dtbo taudac-overlay.dts.i
 
-taudac.eep: taudac-eeprom.txt taudac.dtbo
+taudac.eep: taudac-eeprom.txt taudac.dtbo taudac-info.txt
 	@$(call ok,"Building EEPROM image...")
-	$(EEPMAKE) taudac-eeprom.txt taudac.eep taudac.dtbo
+	$(EEPMAKE) taudac-eeprom.txt taudac.eep taudac.dtbo -c taudac-info.txt
 
 eeprom.unlocked:
 	@$(call ok,"Unlocking EEPROM...")
@@ -71,4 +74,5 @@ clean:
 	rm -f *.dtbo
 	rm -f *.eep
 	rm -f eeprom.unlocked
+	rm -f taudac-info.txt
 

.gitignore
@@ -1,4 +1,4 @@
 *.dts.i
 *.dtbo
 *.eep
-
+taudac-info.txt

commit 7f8b9f8d17636ef582f2540403d1c7415dd39f32

make: Declare clean and taudac-info.txt to be PHONY
Fixes the issues where the taudac-info.txt was not updated when flashing
the EEPROM.

Fixes: commit e6ccb51d81bf ("eep: Add commit hash as custom data")

Makefile
@@ -25,6 +25,8 @@ define die
 	(echo $(cred)$(1)$(cend); exit 1)
 endef
 
+.PHONY: clean taudac-info.txt
+
 all: taudac.eep
 
 taudac-info.txt: